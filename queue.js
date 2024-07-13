8225942cbb8d51bc80fcb251d47b683519d08a6d
Nayara Nicacio <nayaranicacio@hotmail.com>
2024-07-13 12:04:16 -0300

    Resolucao do desafio aprofunda

--- queue.js
+++ queue.js
@@ -1,10 +1,33 @@
 class Queue {
   elements
   constructor(){
-    this.elements = []
+    this.elements = [1,5,6,2,3,4,8]
   }
-  
-  // Implementar aqui
+  size
+  constructor(){
+    this.size = []
+    return this.elements.length;
+  }
+  add
+  constructor(){
+    this.add = []
+    add(element) {
+      this.elements.push(element);
+    }
+  }
+  peek
+  constructor(){
+    this.peek = []
+  }
+
+  dequeue
+  constructor(){
+    this.dequeue = []  
+    return this.elements.shift();
+  }
+  }
+
+
 }
 
 module.exports = Queue
--- queue.test.js
+++ queue.test.js
@@ -2,24 +2,28 @@ const Queue = require('./queue')
 
 describe('Queue', () => {
   it('Deve criar uma fila', () => {
-    const queue = new Queue()
 
-    expect(queue).toEqual({ elements: []})
+const queue = new Queue()
+expect(queue).toEqual({ elements: []})
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+const size = new Queue()
+expect(size).toEqual({ size: []})
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+const add = new Queue()
+expect(add).toEqual({ add: []})
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+const peek = new Queue()
+expect(peek).toEqual({ peek: []})
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+const dequeue = new Queue()
+expect(dequeue).toEqual({ dequeue: []})
   })
 })
